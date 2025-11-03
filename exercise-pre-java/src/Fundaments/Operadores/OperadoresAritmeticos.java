2dbf44eb641d72e51902dec9f210e40a87750238
Patrick <patrickcabrera2025@gmail.com>
2025-11-02 20:53:26 -0500

    Operadores aritmeticos y de comparacion adición

---
+++ exercise-pre-java/src/Fundaments/Operadores/OperadoresAritmeticos.java
@@ -0,0 +1,19 @@
+package Fundaments.Operadores;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args){
+        int a = 12;
+        int b = 5;
+        System.out.println("Suma: " + (a+b));
+        System.out.println("Resta: " + (a-b));
+        System.out.println("Multiplicacion: " +(a*b));
+        System.out.println("Division: " + (a/b));
+        System.out.println("Residuo de división: " + (a%b));
+
+        double c = 5.48;
+        double d = 2.5;
+        System.out.println("Division decimal: " + (c/d));
+
+
+    }
+}
---
+++ exercise-pre-java/src/Fundaments/Operadores/OperadoresComparacion.java
@@ -0,0 +1,17 @@
+package Fundaments.Operadores;
+
+public class OperadoresComparacion {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 2;
+
+        //salida
+        System.out.println("Los datos son: " + x + " y " + y + " respectivamente");
+        System.out.println("x == y: " + (x == y));
+        System.out.println("x != y:" + (x != y));
+        System.out.println("x < y: " + (x < y));
+        System.out.println("x > y: " + (x > y));
+        System.out.println("x <= y: " + (x <= y));
+        System.out.println("x >= y: " + (x >=y));
+    }
+}
